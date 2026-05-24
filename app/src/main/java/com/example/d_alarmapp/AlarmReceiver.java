e0512f6d0d2d9d35d943a4f0a4a10a940b08ccd2
damnenby <damnenby@gmail.com>
2026-05-24 12:33:52 +0200

    open alarm screen

--- app/src/main/java/com/example/d_alarmapp/AlarmReceiver.java
+++ app/src/main/java/com/example/d_alarmapp/AlarmReceiver.java
@@ -8,5 +8,14 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (!AlarmHelper.ACTION_ALARM.equals(intent.getAction())) {
+            return;
+        }
+
+        long alarmTimeMillis = intent.getLongExtra(AlarmHelper.EXTRA_ALARM_TIME_MILLIS, 0L);
+        Intent alarmIntent = new Intent(context, AlarmActivity.class);
+        alarmIntent.putExtra(AlarmHelper.EXTRA_ALARM_TIME_MILLIS, alarmTimeMillis);
+        alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(alarmIntent);
     }
 }
