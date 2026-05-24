5a27f385ddca5dbce72cc292abfb0ae6a6eb122b
damnenby <damnenby@gmail.com>
2026-05-24 13:54:20 +0200

    start alarm sound

--- app/src/main/java/com/example/d_alarmapp/AlarmActivity.java
+++ app/src/main/java/com/example/d_alarmapp/AlarmActivity.java
@@ -20,5 +20,7 @@ public class AlarmActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        AlarmSoundPlayer.start(this);
     }
 }
